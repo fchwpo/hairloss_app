20d376c45073fef6640e8c2bf2c84088008539ff
kuhsam <dev.nexonfi@gmail.com>
2024-05-26 21:27:14 +0530

    init: Initial Commit - Add supabase

--- src/pages/api/saveImage.ts
+++ src/pages/api/saveImage.ts
@@ -2,20 +2,20 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { supabase } from '../../lib/supabaseClient';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    res.status(405).end(); // Method Not Allowed
-    return;
-  }
+  if (req.method === 'POST') {
+    const { imgurLink } = req.body;
 
-  const { imgurLink } = req.body;
+    const { data, error } = await supabase
+      .from('images')
+      .insert([{ imgur_url: imgurLink, timestamp: new Date(), prediction: '' }]);
 
-  const { data, error } = await supabase
-    .from('images')
-    .insert([{ imgur_url: imgurLink, timestamp: new Date(), prediction: '' }]);
-
-  if (error) {
-    res.status(500).json({ error: 'Error saving image to database' });
+    if (error) {
+      res.status(500).json({ error: 'Error saving image to database' });
+    } else {
+      res.status(200).json({ data });
+    }
   } else {
-    res.status(200).json({ data });
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
