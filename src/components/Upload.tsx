5141defcebdd890d68b06497eb69f4b5d9e06921
kuhsam <dev.nexonfi@gmail.com>
2024-05-26 22:15:43 +0530

    minor chnages

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -2,6 +2,25 @@ import React, { useState, ChangeEvent, useEffect } from 'react'
 import axios from 'axios'
 import { Dialog } from '@headlessui/react'
 
+const CheckCircleIcon: React.FC = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      stroke='currentColor'
+      className='w-6 h-6 text-green-600'
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2'
+        d='M5 13l4 4L19 7'
+      />
+    </svg>
+  )
+}
+
 const Upload: React.FC = () => {
   const [image, setImage] = useState<File | null>(null)
   const [uploading, setUploading] = useState(false)
@@ -9,6 +28,7 @@ const Upload: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false)
   const [dialogMessage, setDialogMessage] = useState('')
   const [prediction, setPrediction] = useState<string | null>(null)
+  const [fetchingPrediction, setFetchingPrediction] = useState(false)
 
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
@@ -56,24 +76,42 @@ const Upload: React.FC = () => {
       setDialogMessage('Image uploaded and link saved successfully!')
       setIsOpen(true)
 
-      // Start polling for prediction
-      const intervalId = setInterval(async () => {
-        const response = await axios.get(
-          `/api/getPrediction?imgurLink=${imgurLink}`
-        )
-        const { prediction } = response.data
-
-        if (prediction) {
-          setPrediction(prediction)
-          clearInterval(intervalId)
-        }
-      }, 5000)
+      setFetchingPrediction(true)
     } catch (error) {
       console.error('Error uploading image:', error)
       setDialogMessage('Error uploading image. Please try again.')
       setIsOpen(true)
     } finally {
       setUploading(false)
+    }
+  }
+
+  useEffect(() => {
+    if (prediction === null && image !== null && !fetchingPrediction) {
+      handlePrediction()
+    }
+  }, [prediction, image, fetchingPrediction])
+
+  const handlePrediction = async () => {
+    if (!image) return
+
+    const formData = new FormData()
+    formData.append('image', image)
+
+    try {
+      const response = await axios.post('/api/predict', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+
+      const { data } = response
+
+      setPrediction(data.prediction)
+    } catch (error) {
+      console.error('Error fetching prediction:', error)
+    } finally {
+      setFetchingPrediction(false)
     }
   }
 
@@ -99,24 +137,62 @@ const Upload: React.FC = () => {
             className='mt-2 w-full h-auto rounded-lg'
           />
         )}
-        <button
-          onClick={handleUpload}
-          className='mt-4 w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 disabled:opacity-50'
-          disabled={uploading || !image}
-        >
-          {uploading ? 'Uploading...' : 'Upload Image'}
-        </button>
-      </div>
-      {uploading && (
-        <div className='mt-4 w-full bg-gray-200 rounded-full'>
-          <div
-            className='bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full'
-            style={{ width: `${progress}%` }}
+        {!prediction && !uploading && (
+          <button
+            onClick={handleUpload}
+            className='mt-4 w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 disabled:opacity-50'
+            disabled={uploading || !image}
           >
-            {progress}%
+            Upload Image
+          </button>
+        )}
+
+        {uploading && (
+          <div className='mt-4 w-full bg-gray-200 rounded-full'>
+            <div
+              className='bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full'
+              style={{ width: `${progress}%` }}
+            >
+              {progress}%
+            </div>
           </div>
-        </div>
-      )}
+        )}
+
+        {fetchingPrediction && !prediction && (
+          <div className='mt-4 flex items-center justify-center text-blue-600'>
+            <svg
+              className='animate-spin h-5 w-5 mr-3'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+            >
+              <circle
+                className='opacity-25'
+                cx='12'
+                cy='12'
+                r='10'
+                stroke='currentColor'
+                strokeWidth='4'
+              ></circle>
+              <path
+                className='opacity-75'
+                fill='currentColor'
+                d='M4 12a8 8 0 018-8V2.5A1.5 1.5 0 0113.5 1h-3A1.5 1.5 0 019 2.5V4a8 8 0 014 7.33V12h2.5a1.5 1.5 0 010 3h-6a1.5 1.5 0 010-3H12v-2.67A8 8 0 014 12z'
+              ></path>
+            </svg>
+            Fetching prediction...
+          </div>
+        )}
+
+        {prediction && (
+          <div className='mt-6 p-4 bg-green-100 border border-green-200 rounded-md'>
+            <div className='flex items-center'>
+              <CheckCircleIcon />
+              <p className='ml-2 text-green-700'>Prediction: {prediction}</p>
+            </div>
+          </div>
+        )}
+      </div>
 
       <Dialog
         open={isOpen}
@@ -141,15 +217,6 @@ const Upload: React.FC = () => {
           </Dialog.Panel>
         </div>
       </Dialog>
-
-      {prediction && (
-        <div className='mt-6 p-4 bg-green-100 border border-green-200 rounded-md'>
-          <div className='flex items-center'>
-            {/* <CheckCircleIcon className="w-6 h-6 text-green-600" /> */}
-            <p className='ml-2 text-green-700'>Prediction: {prediction}</p>
-          </div>
-        </div>
-      )}
     </div>
   )
 }
